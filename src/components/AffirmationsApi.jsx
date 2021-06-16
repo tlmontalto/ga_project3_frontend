7b7a41959b4299e876d42192d7fda84cd1966eca
Tamir Montalto <tmontalto@MacBook-Pro-2.fios-router.home>
2021-06-16 12:09:34 -0400

    deleted project tracker folder, moved to outer folder

---
+++ src/components/AffirmationsApi.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class affirmationsApi extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            affirmation: [],
+            isLoaded: false,
+        }
+    }
+
+
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
