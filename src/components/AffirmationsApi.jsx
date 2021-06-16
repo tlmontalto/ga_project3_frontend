7b6b1de9ded133f7e2dbcd388abf06bb1d1c47c0
Tamir Montalto <tmontalto@MacBook-Pro-2.fios-router.home>
2021-06-16 14:07:47 -0400

    added content to affirmations

--- src/components/AffirmationsApi.jsx
+++ src/components/AffirmationsApi.jsx
@@ -5,17 +5,38 @@ export default class affirmationsApi extends Component {
         super(props)
 
         this.state = {
-            affirmation: [],
+            affirmation: null,
             isLoaded: false,
         }
     }
 
+    // getAffirmation = () => {
+    //     fetch('https://www.affirmations.dev/')
+    //     .then(res => res.json())
+    //     .then(json => {
+    //         this.setState({
+    //             isLoaded: true,
+    //             affirmation: json,
+    //         })
+    //     })
+    // }
 
+    componentDidMount() {
+        const url = 'https://www.affirmations.dev/'
+        const response = await fetch(url)
+        const data = await response.json()
+        this.setState({affirmation: data})
+        console.log(data);
+    }
 
     render() {
         return (
             <div>
-                
+                {this.state.isLoaded || !this.state.affirmation ? (
+                    <div>Loading...</div>
+                ) : (
+                    <div>{this.state.affirmation}</div>
+                    )}
             </div>
         )
     }
